7f15e950e4e0150e54d7b8bbee6aa62f5a5c55a2
Shubham Singh <Shubham16cse06@gmail.com>
2021-04-04 00:00:52 +0530

    challenge of importing node.js core module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require("fs");
+const appendFileSync = require("fs");
 
 fs.writeFileSync("notes.txt", "My name is shubham.");
+fs.appendFileSync("notes.txt", " This is append file sync.");
