9ce0261d3fcbc8d50643b1ef226b4486bfa89458
Shubham Singh <Shubham16cse06@gmail.com>
2021-04-08 23:48:42 +0530

    argument parsing with yargs part-2

--- app.js
+++ app.js
@@ -8,18 +8,44 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
   },
 });
 
 // creating a remove command
 yargs.command({
   command: "remove",
-  describe: "Remove a new command",
+  describe: "Remove a note",
   handler: function () {
     console.log("Removing a note!");
   },
 });
 
-console.log(yargs.argv);
+// creating a list command
+yargs.command({
+  command: "list",
+  describe: "listing the notes",
+  handler: function () {
+    console.log("List the notes!");
+  },
+});
+
+// creating a read command
+yargs.command({
+  command: "read",
+  describe: "read the notes",
+  handler: function () {
+    console.log("Read the notes!");
+  },
+});
+
+// console.log(yargs.argv);
+yargs.parse();
