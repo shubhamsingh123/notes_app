84f810f42533751ee7ed89ef2abd2aad4dc060a5
Shubham Singh <Shubham16cse06@gmail.com>
2021-05-13 19:20:53 +0530

    Working with appendFileSync inside the fileSystem

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
 const fileSystem = require("fs");
 
-fileSystem.writeFileSync(
-  "notes.txt",
-  "Hello This is shubham , And i am from INDIA"
-);
+fileSystem.appendFileSync("notes.txt", " Hello THis is shubham!!!");
