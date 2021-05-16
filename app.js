4f25871ee3b7c545460e5e8af07ad097f6ec64b6
Shubham Singh <Shubham16cse06@gmail.com>
2021-05-16 08:03:28 +0530

    Customize yargs version

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
 console.log(chalk.yellow.inverse.bold("Hello Shubham , How are you!!!"));
 
-const command = process.argv[2];
+//Customize yargs version
+yargs.version("2.2.2");
 
-console.log(process.argv);
-
-if (command === "add") {
-  console.log("Adding notes...");
-} else if (command === "remove") {
-  console.log("Removing notes...");
-} else {
-  console.log("Hello , Shubham");
-}
+// console.log(process.argv);
+console.log(yargs.argv);
